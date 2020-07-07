baf43f82ba27bf4ff4444a3e88e196e6fea8fcc4
Ali Labib <alilabib65@gmail.com>
2020-07-07 05:42:46 +0200

    data and folders

---
+++ database/migrations/2020_07_07_013844_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->nullable();	
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->enum('enable_tax',['0','1'])->default('0');
+            $table->string('tax')->nullable();
+            $table->string('sub_total')->nullable();
+            $table->string('total')->nullable();
+            $table->string('status')->nullable();
+            $table->string('date')->nullable();
+            $table->string('cancel_status')->nullable();
+            $table->string('payment_type')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}

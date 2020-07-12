e138de0096f01636e3a987c48842721bf56c3722
Ali Labib <alilabib65@gmail.com>
2020-07-12 02:09:38 +0200

    provider request

---
+++ app/Http/Requests/Api/Auth/ProviderRegisterRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Api\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProviderRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'=>'required',
+            'email'=>'required|unique:users,email',
+            'phone'=>'required|unique:users,phone',
+            'city'=>'required',
+            'password'=>'required|confirmed|min:4',
+            'residence_img'=>'required',
+            'license_img'=>'required'
+        ];
+    }
+}

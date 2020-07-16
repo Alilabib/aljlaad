eb56a160491b2dcff9d7226e13bbe8ca45201ec0
Ali Labib <alilabib65@gmail.com>
2020-07-16 02:06:37 +0200

    push data

---
+++ database/seeds/Adminseeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+
+class Adminseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Admin::create([
+            'name'=>'Admin',
+            'email'=>'admin@site.com',
+            'password'=>'123456789'
+        ]);
+    }
+}
